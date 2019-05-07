d255f7d943fbcbf64f541b64bfefbb10dde01056
shihjufu <42971074+shihjufu@users.noreply.github.com>
2019-05-07 17:07:26 -0400

    Add files via upload

---
+++ studio/project2/java.js
@@ -0,0 +1,458 @@
+$(document).ready(function(){
+
+//sphereone//
+
+function c1(){
+	var randomRotateY = Math.random()* 180;
+
+	var rotateY = "rotate(0deg) " + "rotateY(" + Math.floor(randomRotateY) + "deg)";
+
+	$(".c1").css("transform",rotateY)
+}
+
+function c2(){
+	var randomRotateY = Math.random()* 180;
+
+	var rotateY = "rotate(30deg) " + "rotateY(" + Math.floor(randomRotateY) + "deg)";
+
+	$(".c2").css("transform",rotateY)
+}
+
+function c3(){
+	var randomRotateY = Math.random()* 180;
+
+	var rotateY = "rotate(60deg) " + "rotateY(" + Math.floor(randomRotateY) + "deg)";
+
+	$(".c3").css("transform",rotateY)
+}
+
+function c4(){
+	var randomRotateY = Math.random()* 180;
+
+	var rotateY = "rotate(90deg) " + "rotateY(" + Math.floor(randomRotateY) + "deg)";
+
+	$(".c4").css("transform",rotateY)
+}
+
+function c5(){
+	var randomRotateY = Math.random()* 180;
+
+	var rotateY = "rotate(120deg) " + "rotateY(" + Math.floor(randomRotateY) + "deg)";
+
+	$(".c5").css("transform",rotateY)
+}
+
+function c6(){
+	var randomRotateY = Math.random()* 180;
+
+	var rotateY = "rotate(150deg) " + "rotateY(" + Math.floor(randomRotateY) + "deg)";
+
+	$(".c6").css("transform",rotateY)
+}
+
+//spheretwo//
+
+function c7(){
+	var randomRotateY = Math.random()* 180;
+
+	var rotateY = "rotate(150deg) " + "rotateY(" + Math.floor(randomRotateY) + "deg)";
+
+	$(".c7").css("transform",rotateY)
+}
+
+function c8(){
+	var randomRotateY = Math.random()* 180;
+
+	var rotateY = "rotate(150deg) " + "rotateY(" + Math.floor(randomRotateY) + "deg)";
+
+	$(".c8").css("transform",rotateY)
+}
+
+function c9(){
+	var randomRotateY = Math.random()* 180;
+
+	var rotateY = "rotate(150deg) " + "rotateY(" + Math.floor(randomRotateY) + "deg)";
+
+	$(".c9").css("transform",rotateY)
+}
+
+function c10(){
+	var randomRotateY = Math.random()* 180;
+
+	var rotateY = "rotate(150deg) " + "rotateY(" + Math.floor(randomRotateY) + "deg)";
+
+	$(".c10").css("transform",rotateY)
+}
+
+
+function c11(){
+	var randomRotateY = Math.random()* 180;
+
+	var rotateY = "rotate(150deg) " + "rotateY(" + Math.floor(randomRotateY) + "deg)";
+
+	$(".c11").css("transform",rotateY)
+}
+
+function c12(){
+	var randomRotateY = Math.random()* 180;
+
+	var rotateY = "rotate(150deg) " + "rotateY(" + Math.floor(randomRotateY) + "deg)";
+
+	$(".c12").css("transform",rotateY)
+}
+
+//spherethree//
+
+function c13(){
+	var randomRotateY = Math.random()* 180;
+
+	var rotateY = "rotate(30deg) " + "rotateY(" + Math.floor(randomRotateY) + "deg)";
+
+	$(".c13").css("transform",rotateY)
+}
+
+function c14(){
+	var randomRotateY = Math.random()* 180;
+
+	var rotateY = "rotate(30deg) " + "rotateY(" + Math.floor(randomRotateY) + "deg)";
+
+	$(".c14").css("transform",rotateY)
+}
+
+function c15(){
+	var randomRotateY = Math.random()* 180;
+
+	var rotateY = "rotate(30deg) " + "rotateY(" + Math.floor(randomRotateY) + "deg)";
+
+	$(".c15").css("transform",rotateY)
+}
+
+function c16(){
+	var randomRotateY = Math.random()* 180;
+
+	var rotateY = "rotate(30deg) " + "rotateY(" + Math.floor(randomRotateY) + "deg)";
+
+	$(".c16").css("transform",rotateY)
+}
+
+function c17(){
+	var randomRotateY = Math.random()* 180;
+
+	var rotateY = "rotate(30deg) " + "rotateY(" + Math.floor(randomRotateY) + "deg)";
+
+	$(".c17").css("transform",rotateY)
+}
+
+function c18(){
+	var randomRotateY = Math.random()* 180;
+
+	var rotateY = "rotate(30deg) " + "rotateY(" + Math.floor(randomRotateY) + "deg)";
+
+	$(".c18").css("transform",rotateY)
+}
+
+//spherefour//
+
+function c19(){
+	var randomRotateY = Math.random()* 180;
+
+	var rotateY = "rotate(0deg) " + "rotateY(" + Math.floor(randomRotateY) + "deg)";
+
+	$(".c19").css("transform",rotateY)
+}
+
+function c20(){
+	var randomRotateY = Math.random()* 180;
+
+	var rotateY = "rotate(30deg) " + "rotateY(" + Math.floor(randomRotateY) + "deg)";
+
+	$(".c20").css("transform",rotateY)
+}
+
+function c21(){
+	var randomRotateY = Math.random()* 180;
+
+	var rotateY = "rotate(60deg) " + "rotateY(" + Math.floor(randomRotateY) + "deg)";
+
+	$(".c21").css("transform",rotateY)
+}
+
+function c22(){
+	var randomRotateY = Math.random()* 180;
+
+	var rotateY = "rotate(90deg) " + "rotateY(" + Math.floor(randomRotateY) + "deg)";
+
+	$(".c22").css("transform",rotateY)
+}
+
+function c23(){
+	var randomRotateY = Math.random()* 180;
+
+	var rotateY = "rotate(120deg) " + "rotateY(" + Math.floor(randomRotateY) + "deg)";
+
+	$(".c23").css("transform",rotateY)
+}
+
+function c24(){
+	var randomRotateY = Math.random()* 180;
+
+	var rotateY = "rotate(150deg) " + "rotateY(" + Math.floor(randomRotateY) + "deg)";
+
+	$(".c24").css("transform",rotateY)
+}
+
+//spherefive//
+
+function c25(){
+	var randomRotateY = Math.random()* 180;
+
+	var rotateY = "rotate(0deg) " + "rotateY(" + Math.floor(randomRotateY) + "deg)";
+
+	$(".c25").css("transform",rotateY)
+}
+
+function c26(){
+	var randomRotateY = Math.random()* 180;
+
+	var rotateY = "rotate(30deg) " + "rotateY(" + Math.floor(randomRotateY) + "deg)";
+
+	$(".c26").css("transform",rotateY)
+}
+
+function c27(){
+	var randomRotateY = Math.random()* 180;
+
+	var rotateY = "rotate(60deg) " + "rotateY(" + Math.floor(randomRotateY) + "deg)";
+
+	$(".c27").css("transform",rotateY)
+}
+
+function c28(){
+	var randomRotateY = Math.random()* 180;
+
+	var rotateY = "rotate(90deg) " + "rotateY(" + Math.floor(randomRotateY) + "deg)";
+
+	$(".c28").css("transform",rotateY)
+}
+
+function c29(){
+	var randomRotateY = Math.random()* 180;
+
+	var rotateY = "rotate(120deg) " + "rotateY(" + Math.floor(randomRotateY) + "deg)";
+
+	$(".c29").css("transform",rotateY)
+}
+
+function c30(){
+	var randomRotateY = Math.random()* 180;
+
+	var rotateY = "rotate(150deg) " + "rotateY(" + Math.floor(randomRotateY) + "deg)";
+
+	$(".c30").css("transform",rotateY)
+}
+
+//spheresix//
+
+function c31(){
+	var randomRotateY = Math.random()* 180;
+
+	var rotateY = "rotate(30deg) " + "rotateY(" + Math.floor(randomRotateY) + "deg)";
+
+	$(".c31").css("transform",rotateY)
+}
+
+function c32(){
+	var randomRotateY = Math.random()* 180;
+
+	var rotateY = "rotate(30deg) " + "rotateY(" + Math.floor(randomRotateY) + "deg)";
+
+	$(".c32").css("transform",rotateY)
+}
+
+function c33(){
+	var randomRotateY = Math.random()* 180;
+
+	var rotateY = "rotate(30deg) " + "rotateY(" + Math.floor(randomRotateY) + "deg)";
+
+	$(".c33").css("transform",rotateY)
+}
+
+function c34(){
+	var randomRotateY = Math.random()* 180;
+
+	var rotateY = "rotate(30deg) " + "rotateY(" + Math.floor(randomRotateY) + "deg)";
+
+	$(".c34").css("transform",rotateY)
+}
+
+function c35(){
+	var randomRotateY = Math.random()* 180;
+
+	var rotateY = "rotate(30deg) " + "rotateY(" + Math.floor(randomRotateY) + "deg)";
+
+	$(".c35").css("transform",rotateY)
+}
+
+function c36(){
+	var randomRotateY = Math.random()* 180;
+
+	var rotateY = "rotate(30deg) " + "rotateY(" + Math.floor(randomRotateY) + "deg)";
+
+	$(".c36").css("transform",rotateY)
+}
+
+//sphereseven//
+
+function c37(){
+	var randomRotateY = Math.random()* 180;
+
+	var rotateY = "rotate(150deg) " + "rotateY(" + Math.floor(randomRotateY) + "deg)";
+
+	$(".c37").css("transform",rotateY)
+}
+
+function c38(){
+	var randomRotateY = Math.random()* 180;
+
+	var rotateY = "rotate(150deg) " + "rotateY(" + Math.floor(randomRotateY) + "deg)";
+
+	$(".c38").css("transform",rotateY)
+}
+
+
+function c39(){
+	var randomRotateY = Math.random()* 180;
+
+	var rotateY = "rotate(150deg) " + "rotateY(" + Math.floor(randomRotateY) + "deg)";
+
+	$(".c39").css("transform",rotateY)
+}
+
+function c40(){
+	var randomRotateY = Math.random()* 180;
+
+	var rotateY = "rotate(150deg) " + "rotateY(" + Math.floor(randomRotateY) + "deg)";
+
+	$(".c40").css("transform",rotateY)
+}
+
+function c41(){
+	var randomRotateY = Math.random()* 180;
+
+	var rotateY = "rotate(150deg) " + "rotateY(" + Math.floor(randomRotateY) + "deg)";
+
+	$(".c41").css("transform",rotateY)
+}
+
+function c42(){
+	var randomRotateY = Math.random()* 180;
+
+	var rotateY = "rotate(150deg) " + "rotateY(" + Math.floor(randomRotateY) + "deg)";
+
+	$(".c42").css("transform",rotateY)
+}
+
+//sphereeight//
+
+function c43(){
+	var randomRotateY = Math.random()* 180;
+
+	var rotateY = "rotate(0deg) " + "rotateY(" + Math.floor(randomRotateY) + "deg)";
+
+	$(".c43").css("transform",rotateY)
+}
+
+function c44(){
+	var randomRotateY = Math.random()* 180;
+
+	var rotateY = "rotate(30deg) " + "rotateY(" + Math.floor(randomRotateY) + "deg)";
+
+	$(".c44").css("transform",rotateY)
+}
+
+function c45(){
+	var randomRotateY = Math.random()* 180;
+
+	var rotateY = "rotate(60deg) " + "rotateY(" + Math.floor(randomRotateY) + "deg)";
+
+	$(".c45").css("transform",rotateY)
+}
+
+function c46(){
+	var randomRotateY = Math.random()* 180;
+
+	var rotateY = "rotate(90deg) " + "rotateY(" + Math.floor(randomRotateY) + "deg)";
+
+	$(".c46").css("transform",rotateY)
+}
+
+function c47(){
+	var randomRotateY = Math.random()* 180;
+
+	var rotateY = "rotate(120deg) " + "rotateY(" + Math.floor(randomRotateY) + "deg)";
+
+	$(".c47").css("transform",rotateY)
+}
+
+function c48(){
+	var randomRotateY = Math.random()* 180;
+
+	var rotateY = "rotate(150deg) " + "rotateY(" + Math.floor(randomRotateY) + "deg)";
+
+	$(".c48").css("transform",rotateY)
+}
+
+setInterval(function(){
+	c1()
+	c2()
+	c3()
+	c4()
+	c5()
+	c6()
+	c7()
+	c8()
+	c9()
+	c10()
+	c11()
+	c12()
+	c13()
+	c14()
+	c15()
+	c16()
+	c17()
+	c18()
+	c19()
+	c20()
+	c21()
+	c22()
+	c23()
+	c24()
+	c25()
+	c26()
+	c27()
+	c28()
+	c29()
+	c30()
+	c31()
+	c32()
+	c33()
+	c34()
+	c35()
+	c36()
+	c37()
+	c38()
+	c39()
+	c40()
+	c41()
+	c42()
+	c43()
+	c44()
+	c45()
+	c46()
+	c47()
+	c48()
+},1000)
+
+
+
+})
